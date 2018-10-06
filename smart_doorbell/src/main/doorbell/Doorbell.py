905e2b6c7ea7734e64417fa97510f4dde19cb299
Lydia Ralph <lydia.ralph@bbc.co.uk>
2018-10-06 19:03:40 +0100

    Big restructuring of main class

--- smart_doorbell/src/main/doorbell/Doorbell.py
+++ smart_doorbell/src/main/doorbell/Doorbell.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from gpiozero import Button, MotionSensor
+from gpiozero import MotionSensor
 from time import sleep
 from datetime import datetime
 import os
@@ -13,49 +13,92 @@ from doorbell.Resident import Resident
 import logging
 
 
+class Doorbell:
+
+    def __init__(self):
+        self.log = self.set_up_logging()
+        self.residents = self.set_up_residents()
+        self.microphone = MicrophoneImpl()
+        self.motion_sensor = MotionSensor(4)
+        self.speaker = Speaker()
+        print("SmartDoorbell application is ready. Logs will now be located at ", self.log.basicConfig())
+
+    @staticmethod
+    def set_up_logging():
+        log_directory = '../logging'
+        logging_file_path = 'smart_doorbell.full.log'
+        Doorbell.refresh_logs(log_directory, logging_file_path)
+        logging_file_name = "{}/{}".format(log_directory, logging_file_path)
+        print(logging_file_name)
+        logging.basicConfig(filename=logging_file_name, level=logging.DEBUG)
+        return logging
+
+    @staticmethod
+    def set_up_residents():
+        resident_matt = Resident('Matt', ['matt.wav', 'matthew.wav', 'mr_ralph.wav'])
+        resident_lydia = Resident('Lydia', ['lydia.wav', 'mrs_ralph.wav'])
+        resident_anyone = Resident('Anyone', ['anyone.wav', 'idontmind.wav'])
+        residents = [resident_matt, resident_lydia, resident_anyone]
+        residents_list_string = ', '.join(str(x.text_name) for x in residents)
+        logging.debug("Registered residents: " + residents_list_string)
+        return residents
+
+    @staticmethod
+    def refresh_logs(log_directory, logging_file_path):
+        print("Refreshing logging files")
+        time_now = datetime.now().time()
+        print("time: ", time_now)
+        archive_log_file_path = "archive-{}-{}".format(time_now, logging_file_path)
+        if os.path.exists("{}/{}".format(log_directory, logging_file_path)):
+            print("Previous log file exists")
+            full_path = "{}/{}".format(log_directory, logging_file_path)
+            archive_path = "{}/{}".format(log_directory, archive_log_file_path)
+            print("Archiving previous log file")
+            copyfile(full_path, archive_path)
+            print("Finished archiving log file")
+            os.remove(full_path)
+            print("Finished removing old log file")
+        else:
+            print("Creating log directory")
+            os.mkdir(log_directory)
+
+    def doorbell_response(self):
+        self.speaker.speak_resident_name()
+        resident_name_audio_text = self.microphone.recognise_speech()
+        self.speaker.speak_visitor_name()
+        visitor_name_audio_text = self.microphone.recognise_speech()
+        resident_recognised = False
+        for resident in self.residents:
+            if resident.requested_name_matches_this_resident(resident_name_audio_text):
+                resident_recognised = True
+                logging.info(resident.text_name, ' was requested by the visitor')
+                resident.alert_visitor_at_door(visitor_name_audio_text)
+
+        return resident_recognised
+
+
 def main():
-    # GENERAL CONFIG
-    log_directory = '../logging'
-    logging_file_path = 'smart_doorbell.full.log'
-    refresh_logs(log_directory, logging_file_path)
+    doorbell = Doorbell()
 
-    logging_file_name = "{}/{}".format(log_directory,logging_file_path)
-    print(logging_file_name)
-    logging.basicConfig(filename=logging_file_name, level=logging.DEBUG)
-
-    # COMPONENTS
-    # doorbell_button = Button(4, pull_up=False)
-    motion_sensor = MotionSensor(4)
-    speaker = Speaker()
-    microphone = MicrophoneImpl()
-
-    # RESIDENTS
-    resident_matt = Resident('Matt', ['matt.wav', 'matthew.wav', 'mr_ralph.wav'])
-    resident_lydia = Resident('Lydia', ['lydia.wav', 'mrs_ralph.wav'])
-    resident_anyone = Resident('Anyone', ['anyone.wav', 'idontmind.wav'])
-    residents = [resident_matt, resident_lydia, resident_anyone]
-    residents_list_string = ', '.join(str(x.text_name) for x in residents)
-    logging.debug("Registered residents: " + residents_list_string)
-
-    print("SmartDoorbell application is ready. Logs will now be located at ", logging_file_name)
     while True:
-        motion_sensor.wait_for_motion()
+        #doorbell.motion_sensor.wait_for_motion()
+        sleep(10)
         try:
-            logging.info("Pressed!")
-            resident_recognised = False
-            speaker.speak_hello()
-            resident_recognised = doorbell_response(microphone, resident_recognised, residents, speaker)
+            logging.info("Somebody is at the door")
+            doorbell.speaker.speak_hello()
+            resident_recognised = doorbell.doorbell_response()
 
             # Try again
             if not resident_recognised:
                 logging.warning("Requested name was not recognised: trying again")
-                speaker.speak_not_recognised()
-                resident_recognised = doorbell_response(microphone, resident_recognised, residents, speaker)
+                doorbell.speaker.speak_not_recognised()
+                resident_recognised = doorbell.doorbell_response()
 
             # Default: alert everyone
             if not resident_recognised:
-                logging.warning("Requested name was not recognised: requesting anyone to answer the door")
-                resident_anyone.request_answer_door()
+                logging.warning("Requested name was not recognised: sending general alert")
+                for resident in doorbell.residents:
+                    resident.request_answer_door()
 
         # TODO: Decide what to do about exceptions.
         except Exception as e:
@@ -63,42 +106,9 @@ def main():
 
         # Finished: don't want doorbell inactive if error occurs
         finally:
-        # Avoid multiple triggers for same visitor
+            # Avoid multiple triggers for same visitor
             sleep(120)
 
-
-def doorbell_response(microphone, resident_recognised, residents, speaker):
-    speaker.speak_resident_name()
-    resident_name_audio_text = microphone.recognise_speech()
-    speaker.speak_visitor_name()
-    visitor_name_audio_text = microphone.recognise_speech()
-    for resident in residents:
-        if resident.requested_name_matches_this_resident(resident_name_audio_text):
-            resident_recognised = True
-            logging.info(resident.text_name, ' was requested by the visitor')
-            resident.alert_visitor_at_door(visitor_name_audio_text)
-
-    return resident_recognised
-
-def refresh_logs(log_directory, logging_file_path):
-    print("Refreshing logging files")
-
-    time_now = datetime.now().time()
-    print("time: ", time_now)
-    archive_log_file_path = "archive-{}-{}".format(time_now, logging_file_path)
-    if os.path.exists("{}/{}".format(log_directory,logging_file_path)):
-        print("Previous log file exists")
-        full_path = "{}/{}".format(log_directory,logging_file_path)
-        archive_path = "{}/{}".format(log_directory,archive_log_file_path)
-        print("Archiving previous log file")
-        copyfile(full_path, archive_path)
-        print("Finished archiving log file")
-        os.remove(full_path)
-        print("Finished removing old log file")
-    else:
-        print("Creating log directory")
-        os.mkdir(log_directory)
-    
 
 if __name__ == "__main__":
     main()
--- smart_doorbell/src/main/doorbell/Twitter.py
+++ smart_doorbell/src/main/doorbell/Twitter.py
@@ -4,15 +4,15 @@ from configparser import ConfigParser
 
 
 class TwitterImpl:
+    config_location = '../resources'
 
-    def __init__(self, config_location, resident_name):
+    def __init__(self, resident_name):
         config = ConfigParser()
         
-        config.read(config_location + '/twitter.properties')
+        config.read(self.config_location + '/twitter.properties')
 
-        con_key = config('APP','twitter_consumer_api_key')
+        con_key = config('APP', 'twitter_consumer_api_key')
         con_sec_key = config.get('APP', 'twitter_consumer_api_secret_key')
-        # TODO: Make it specific to each resident
         access_token = config.get(resident_name, 'twitter_access_token')
         access_token_secret = config.get(resident_name, 'twitter_access_token_secret')
         
@@ -26,12 +26,12 @@ class TwitterImpl:
         self.user_id = config.get(resident_name, 'user_id')
 
     def get_statuses(self):
-        statuses=self.api.GetUserTimeline(user_id=self.user_id)
+        statuses = self.api.GetUserTimeline(user_id=self.user_id)
         # TODO: Change to log
         print([s.text for s in statuses])
 
     def post_direct_message(self, message):
-        self.api.PostDirectMessage(message,user_id=self.user_id)
+        self.api.PostDirectMessage(message, user_id=self.user_id)
         # TODO: Change to log
         print("Successfully sent message to Twitter user", self.user_id)
 
@@ -40,7 +40,6 @@ class TwitterImpl:
 
 
 if __name__ == '__main__':
-    config_location = '/home/pi/Dev/iot_doorbell/smart_doorbell/src/main/resources'
-    t = TwitterImpl(config_location, 'LYDIA')
+    t = TwitterImpl('LYDIA')
     t.get_statuses()
     t.post_direct_message('third test')
